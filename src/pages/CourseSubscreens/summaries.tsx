fe41b7b0342e5fa69be8d4f3e2a37a777ddc4698
Dariel Sevilla <darielsevilla@gmail.com>
2024-11-27 21:29:46 -0600

    perfil JA, monitoreo, y resumenes funcionales

--- src/pages/CourseSubscreens/summaries.tsx
+++ src/pages/CourseSubscreens/summaries.tsx
@@ -1,4 +1,7 @@
 import { summaries } from "@/data/data"
+import {useState, useEffect} from "react"
+import axios from "axios";
+import Spinner from 'react-bootstrap/Spinner'
 const testCase = () =>{
     return(
         <>
@@ -7,10 +10,41 @@ const testCase = () =>{
     );
 }
 export function SummaryWindow(){
+    const [listSum, setListSum] = useState([]);
+    const [load, setLoad] = useState(0);
+    const [load2, setLoad2] = useState(0);
+    let url = "http://localhost:3001/getSummaries";
+    const item = localStorage.getItem("currentCourse")
+    const loadSummaries = async () =>{
+        try{
+            let url = "http://localhost:3001/getSummaries";
+        const response = await axios.get(url, {
+                params: {
+                    course_id : item ? item : ""
+                }
+            })
+
+            const list = response.data.resultado ? response.data.resultado : [];
+            setListSum(list)
+            
+        }catch(error){
+            
+        }
+        setLoad2(1);
+        
+    }
+    useEffect(()=>{
+        loadSummaries();
+    },[])
+    if(load2 == 0){
+        return(<><div className="container flexVertical loginWindow ">
+            <Spinner animation="border" className="whitetxt margint"/>
+            </div></>)
+    }
     return(<>
     <div className="container whitetxt">
         <h1><b>Resumenes</b></h1>
-        {(summaries.summaries).map((summary)=><div key={summary.unitNum}><div className="titleSummaries margin-5pc"><h4><b>Unidad {summary.unitNum}</b></h4></div>
+        {(listSum).map((summary : any)=><div key={summary.unit_num}><div className="titleSummaries margin-5pc"><h4><b>Unidad {summary.unit_num}</b></h4></div>
         <div className="summaries"><p>{summary.summary}</p></div></div>)}  
     </div></>);
 }
